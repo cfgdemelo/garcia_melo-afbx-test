fc2bd25986230e7dde4e39f9343f66be7c0a6eea
Caio Melo <caio.melo@castecnologia.com.br>
2018-02-21 14:46:13 -0300

    Fixing roboto font family error

--- App.js
+++ App.js
@@ -6,10 +6,23 @@ import {
 } from 'react-navigation';
 import { Image } from 'react-native';
 
-class HomeScreen extends Component {
+export default class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
   static navigationOptions = {
     title: 'Home'
   };
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+    });
+    this.setState({ loading: false });
+  }
 
   render() {
     return (
@@ -17,7 +30,7 @@ class HomeScreen extends Component {
         <Header>
         </Header>
         <Content>
-          <Image source={{uri: 'https://affiniboxclub.com.br/wp-content/uploads/2018/01/logo_affinibox.png'}} style={{height: 200, width: null, flex: 1}}/>
+          <Image source={{uri: 'https://affiniboxclub.com.br/wp-content/uploads/2018/01/logo_affinibox.png'}} style={{height: 100, width: null, flex: 1}}/>
           <Button full={true} onPress={this._handlePress}>
             <Text>Ir para o Feed</Text>
           </Button>
