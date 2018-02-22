6fab0cdb14636b24d1b1f417c9bf4974383138bd
Caio Melo <cfgdemelo@gmail.com>
2018-02-22 00:26:53 -0300

    Fetching data from URL, creating info screen, and starting printing real data

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, Footer, FooterTab, Button, Icon, Body, Title, Text } from 'native-base';
+import { ActivityIndicator, View, ListView } from 'react';
+import { Container, Header, Content, Footer, FooterTab, Button, Icon, Body, Title, Text, Card, CardItem, Thumbnail, Left, Right } from 'native-base';
 import { StackNavigator } from 'react-navigation';
-import { Image } from 'react-native';
+import { Image, Dimensions } from 'react-native';
 import { Font } from 'expo';
+
+const windowWidth = Dimensions.get('window').width;
+const windowWidthHalf = Dimensions.get('window').width / 2;
+var offers = [];
 
 class HomeScreen extends Component {
   static navigationOptions = {
@@ -15,25 +20,151 @@ class HomeScreen extends Component {
         <Header>
         </Header>
         <Content>
-          <Image source={{uri: 'https://affiniboxclub.com.br/wp-content/uploads/2018/01/logo_affinibox.png'}} style={{height: 100, width: null, flex: 1}}/>
-          <Button full={true} onPress={this._handlePress}>
+          <Image source={{uri: 'https://affiniboxclub.com.br/wp-content/uploads/2018/01/logo_affinibox.png'}} style={{height: 100, width: windowWidth, flex: 1, justifyContent: "center"}}/>
+          <Button full={true} onPress={() => this.props.navigation.navigate('Feed')}>
             <Text>Ir para o Feed</Text>
           </Button>
-          <Button info full={true}>
+          <Button info full={true} onPress={() => this.props.navigation.navigate('Info')}>
             <Text>Informações</Text>
           </Button>
         </Content>
       </Container>
     );
   }
+}
 
-  _handlePress = () => {
-    this.props.navigation.navigate('Feed');
+class InfoScreen extends Component {
+  static navigationOptions = {
+    title: 'Info'
+  };
+
+  render() {
+    return (
+      <Container>
+          <Content>
+            <Card>
+              <CardItem cardBody>
+                <Image source={{uri: "http://cfgdemelo.com.br/img/profile.jpg"}} style={{height: 300, width: null, flex: 1}}/>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Text>Feito por Caio Felipe Garcia de Melo</Text>
+                </Left>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button>
+                    <Text>https://github.com/cfgdemelo/garcia_melo-afbx-test</Text>
+                  </Button>
+                </Left>
+              </CardItem>
+            </Card>
+          </Content>
+        </Container>
+    );
   }
 }
 
-export default StackNavigator({
+export class FeedScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true
+    }
+  }
+  componentDidMount() {
+    return fetch('https://sandbox-api.lomadee.com/v2/15072100846841ecfe32c/offer/_bestsellers?sourceId=35850224')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      for (item in responseJson) {
+        var offerItem = responseJson.offers;
+        var offerItemLength = offerItem.length;
+        for (var i = 0; i < offerItemLength; i++) {
+          offers.push(offerItem[i]);
+        }
+      }
+      console.log(offers.length)
+
+      this.setState({
+        isLoading: false
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  renderCards() {
+    return offers.map((item) => {
+      return {
+        
+      }
+    });
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <Image source={{uri: 'http://thinkfuture.com/wp-content/uploads/2013/10/loading_spinner.gif'}} style={{height: 100, width: windowWidth, flex: 1, justifyContent: "center"}}/>
+      );
+    }
+
+    var offersLenght = offers.length;
+    for (var i = 0; i < offersLenght; i++) {
+      return (
+        <Container>
+          <Content>
+            <Card>
+              <CardItem cardBody>
+                <Image source={{uri: offers[i].thumbnail}} style={{height: 100, width: null, flex: 1}}/>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Icon active name="phone-portrait" />
+                </Left>
+                <Body>
+                  <Text>{offers[i].product.shortName}</Text>
+                </Body>
+                <Right>
+                </Right>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Icon active name="pricetag" />
+                </Left>
+                <Body>
+                  <Text>R${offers[i].price}</Text>
+                </Body>
+                <Right>
+                </Right>
+              </CardItem>
+            </Card>
+          </Content>
+        </Container>
+      );
+    }
+  }
+}
+
+const RootStack =  StackNavigator(
+  {
   Home: {
+    screen: HomeScreen,
+  },
+  Feed: {
     screen: FeedScreen,
   },
-});
+  Info: {
+    screen: InfoScreen,
+  }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
